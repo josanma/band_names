dbe7eacc77fdbb16d9f9e782e182126af7950b18
josanma <j.sanma@hotmail.com>
2021-04-13 22:09:17 -0400

    add inital page 'Home'

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:band_names/pages/home.page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,16 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'home',
+      routes: {
+        'home' : (_) => HomePage()
+      },
     );
   }
 }
