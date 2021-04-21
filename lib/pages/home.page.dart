718ca597f7eaa7c307dd6dbee773dcc6cff57ab6
josanma <j.sanma@hotmail.com>
2021-04-20 22:14:01 -0400

    addBandToList

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:band_names/models/band.model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -23,10 +26,16 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(color: Colors.black54),
         ),
         backgroundColor: Colors.white,
+        elevation: 1,
       ),
       body: ListView.builder(
           itemCount: bands.length,
           itemBuilder: (context, i) => _bandTile(bands[i])),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        elevation: 1,
+        onPressed: addNewBand,
+      ),
     );
   }
 
@@ -45,5 +54,58 @@ class _HomePageState extends State<HomePage> {
         print(band.name);
       },
     );
+  }
+
+  void addNewBand() {
+    final textControler = new TextEditingController();
+
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+          context: context,
+          builder: (_) {
+            return CupertinoAlertDialog(
+              title: Text('New band name:'),
+              content: CupertinoTextField(
+                controller: textControler,
+              ),
+              actions: [
+                CupertinoDialogAction(
+                    child: Text('Add'),
+                    onPressed: () => addBandToList(textControler.text)),
+                CupertinoDialogAction(
+                    isDefaultAction: true,
+                    child: Text('Dismiss'),
+                    onPressed: () => Navigator.pop(context))
+              ],
+            );
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('New band name:'),
+              content: TextField(
+                controller: textControler,
+              ),
+              actions: <Widget>[
+                MaterialButton(
+                    child: Text('Add'),
+                    elevation: 10,
+                    textColor: Colors.blueAccent,
+                    onPressed: () => addBandToList(textControler.text))
+              ],
+            );
+          });
+    }
+  }
+
+  void addBandToList(String bandName) {
+    if (bandName.isNotEmpty) {
+      bands.add(Band(id: DateTime.now().toString(), name: bandName, votes: 0));
+    }
+
+    setState(() {});
+    Navigator.pop(context);
   }
 }
